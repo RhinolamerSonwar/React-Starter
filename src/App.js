2e1216bc19af524d0839db8307ba63d5bb6a5682
Rhinolamer <rhinolamer@gmail.com>
2022-06-10 09:17:07 +0300

    react starter code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Router, Route, Link } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { About } from "./components/About";
+import { Home } from "./components/Home";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+}
+export default App;
